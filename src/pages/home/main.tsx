b11c5b2876551eae9682e7d5b5e86a4316354eec
toranj-org <omidpoorali1992@gmail.com>
2022-11-03 14:11:47 +0330

    fix: home style

--- src/pages/home/main.tsx
+++ src/pages/home/main.tsx
@@ -4,7 +4,7 @@ import { Tags } from "./tags";
 export const Home = () => {
 
   return (
-    <div className="h-screen md:grid md:grid-cols-6">
+    <div className="w-full h-full md:grid md:grid-cols-6">
       <Tags className="md:col-span-1" />
       <Notes className="md:col-span-5" />
     </div>
